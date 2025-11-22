0f8ce65d0659db9c83c09e1bacf836b7e27d01df
Your Name <sveducation14@gmail.com>
2025-11-22 15:32:57 +0300

    Commit 4

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
+import sys
 import yt_dlp
 import os
 import argparse
 
-def dl_(url, output_path="downloads/", is_pl=None, ffmpeg_path=None):
-    if not output_path:
-        output_path = "downloads/"
-
+def dl_(url, output_path="downloads/", is_pl=None, ffmpeg_path=None, std=False):
     if not output_path or not os.path.exists(output_path):
+        output_path = "downloads/"
         os.mkdir(output_path)
+
+    if std:
+        output_path = "~/storage/download"
 
     if ("/sets/" in url) or is_pl:
         output_path += "playlists/"
 
-    ffmpath = r"ffmpeg.exe" if not ffmpeg_path else ffmpeg_path
+    ffmpath: str = "ffmpeg.exe"
+    if not ffmpeg_path:
+        if os.path.exists("ffmpeg_path"):
+            ffmpath = open('ffmpeg_path').read().strip()
+    else:
+        ffmpath = ffmpeg_path
+        with open('ffmpeg_path', "w") as f:
+            f.write(ffmpeg_path)
+
     output_path += "%(uploader)s - %(title)s.%(ext)s"
 
     opts = {
@@ -64,16 +74,22 @@ def main():
         "-ff", "--ffmpeg_path",
         default=None
     )
+    pr.add_argument(
+        "-std", "--save_to_dl",
+        default=None
+    )
 
     args = pr.parse_args()
     try:
-        if not args.url:
-            dl_(input())
-            return
-        dl_(args.url,
+        opts = [args.url,
             args.out_path,
             args.playlist,
-            args.ffmpeg_path)
+            args.ffmpeg_path]
+
+        if "-std" in sys.argv:
+            opts += [True]
+
+        dl_(opts)
     except Exception as e:
         print(e)
 
--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 echo "начинается установка..."
 
 pkg install ffmpeg
+which ffmpeg > ffmpeg_path.txt
 
 pip install --upgrade pip
 pip install -r requirements.txt
